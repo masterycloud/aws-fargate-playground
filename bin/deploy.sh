db6c5fd76d412581cd25a60536e4900e04b9eb57
xmcpam <jens@masterycloud.com>
2020-03-11 22:59:20 +0100

    WIP

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,40 +2,85 @@
 
 set -eo pipefail
 
-# Adapt these variables to your needs
+NAME=$1
 HOSTED_ZONE_NAME="devops-hamburg.de"
-CERTIFICATE_ARN="arn:aws:acm:eu-central-1:446914570106:certificate/cc6eb2bd-f88b-47f8-890c-55ae84ad2dd5"
-CONTAINER_PORT="443"
-HEALTHCHECK_PATH="/"
+DOMAIN="${NAME}.${HOSTED_ZONE_NAME}"
 DOCKER_IMAGE="masterycloud/simplehttp"
-SUBNET_TYPE="Public"  # or Private
-ASSIGN_PUBLIC_IP="ENABLED"  # should be ENABLED for public subnets without NAT gateway, otherwise DISABLED
+LOAD_BALANCER_PORT="443"
+HOST_PORT="443"
+CONTAINER_PORT="443"
+DESIRED_COUNT=2
+HOSTED_ZONE_ID=$(aws route53 list-hosted-zones | jq '.HostedZones | map(select(.Name == "'${HOSTED_ZONE_NAME}'.")) | .[0].Id' -r)
+APPLICATION_PROTOCOL="HTTPS"
 
-echo "Deploy VPC..."
-aws cloudformation deploy \
-  --template-file cloudformation/vpc.yaml \
-  --stack-name vpc \
-  --no-fail-on-empty-changeset
-
-VPC_STACK_OUTPUTS=$(aws cloudformation describe-stacks --stack-name vpc | jq .Stacks[0].Outputs)
-VPC_ID=$(echo $VPC_STACK_OUTPUTS | jq '. | map(select(.OutputKey == "VPCId")) | .[].OutputValue' -r)
-SUBNET_A=$(echo $VPC_STACK_OUTPUTS | jq '. | map(select(.OutputKey == "'${SUBNET_TYPE}'Subnet0")) | .[].OutputValue' -r)
-SUBNET_B=$(echo $VPC_STACK_OUTPUTS | jq '. | map(select(.OutputKey == "'${SUBNET_TYPE}'Subnet1")) | .[].OutputValue' -r)
+function get-cf-output-value {
+    STACK_NAME=$1
+    OUTPUT_KEY=$2
+    aws cloudformation describe-stacks \
+        --stack-name ${STACK_NAME} \
+    | jq .[][].Outputs | jq '. | map(select(.OutputKey == "'${OUTPUT_KEY}'")) | .[0].OutputValue' -r
+}
 
 echo
-echo "Deploy Fargate playground..."
+echo ">>> Deploy VPC..."
 aws cloudformation deploy \
-  --template-file cloudformation/fargate.yaml \
-  --stack-name fargate-playground \
-  --parameter-overrides \
-      Image=${DOCKER_IMAGE} \
-      ContainerPort=${CONTAINER_PORT} \
-      HealthCheckPath=${HEALTHCHECK_PATH} \
-      CertificateArn=${CERTIFICATE_ARN} \
-      VpcId=${VPC_ID} \
-      SubnetA=${SUBNET_A} \
-      SubnetB=${SUBNET_B} \
-      AssignPublicIp=${ASSIGN_PUBLIC_IP} \
-      HostedZoneName=${HOSTED_ZONE_NAME} \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --no-fail-on-empty-changeset
+    --template-file cloudformation/vpc.yaml \
+    --stack-name ${NAME}-vpc \
+    --parameter-overrides \
+        VPCName="${NAME}-vpc" \
+    --no-fail-on-empty-changeset
+
+# Retrieve vpc outputs
+VPC_ID=$(get-cf-output-value ${NAME}-vpc "VPCId")
+SUBNET_A=$(get-cf-output-value ${NAME}-vpc "PrivateSubnet0")
+SUBNET_B=$(get-cf-output-value ${NAME}-vpc "PrivateSubnet1")
+
+echo
+echo ">>> Deploy elastic load balancer v2 (ALB)..."
+aws cloudformation deploy \
+    --template-file cloudformation/alb.yaml \
+    --stack-name ${NAME}-alb \
+    --parameter-overrides \
+        VpcId="${VPC_ID}" \
+        SubnetA="${SUBNET_A}" \
+        SubnetB="${SUBNET_B}" \
+        HostedZoneId="${HOSTED_ZONE_ID}" \
+        DomainName="${DOMAIN}" \
+    --no-fail-on-empty-changeset
+
+LOAD_BALANCER_ARN=$(get-cf-output-value ${NAME}-alb LoadBalancer)
+LOAD_BALANCER_SG_ID=$(get-cf-output-value ${NAME}-alb LoadBalancerSecurityGroupId)
+
+
+echo
+echo ">>> Deploy SSL certificate..."
+aws cloudformation deploy \
+    --template-file cloudformation/certificate.yaml \
+    --stack-name ${NAME}-certificate \
+    --parameter-overrides \
+        Subdomain=${NAME} \
+        DomainName=${DOMAIN} \
+    --no-fail-on-empty-changeset
+
+SSL_CERTIFICATE_ARN=$(get-cf-output-value ${NAME}-certificate Certificate)
+
+echo
+echo ">>> Deploy fargate..."
+aws cloudformation deploy \
+    --template-file cloudformation/fargate.yaml \
+    --stack-name ${NAME}-fargate \
+    --parameter-overrides \
+        VpcId="${VPC_ID}" \
+        LoadBalancerArn="${LOAD_BALANCER_ARN}" \
+        SslCertificateArn="${SSL_CERTIFICATE_ARN}" \
+        LoadBalancerPort="${LOAD_BALANCER_PORT}" \
+        HostPort="${HOST_PORT}" \
+        ContainerPort="${CONTAINER_PORT}" \
+        DockerImage="${DOCKER_IMAGE}" \
+        DesiredCount="${DESIRED_COUNT}" \
+        LoadBalancerSecurityGroupId="${LOAD_BALANCER_SG_ID}" \
+        SubnetA="${SUBNET_A}" \
+        SubnetB="${SUBNET_B}" \
+        ApplicationProtocol="${APPLICATION_PROTOCOL}" \
+    --capabilities CAPABILITY_IAM \
+    --no-fail-on-empty-changeset
--- bin/destroy.sh
+++ bin/destroy.sh
@@ -2,9 +2,19 @@
 
 set -eo pipefail
 
-echo "Delete stack fargate-playground..."
-aws cloudformation delete-stack --stack-name fargate-playground
+NAME=$1
+
+echo ">>> Delete stack fargate-playground..."
+aws cloudformation delete-stack --stack-name ${NAME}-fargate
 
 echo
-echo "Delete stack vpc..."
-aws cloudformation delete-stack --stack-name vpc
+echo ">>> Delete stack alb..."
+aws cloudformation delete-stack --stack-name ${NAME}-alb
+
+echo
+echo ">>> Delete stack certificate..."
+aws cloudformation delete-stack --stack-name ${NAME}-certificate
+
+echo
+echo ">>> Delete stack vpc..."
+aws cloudformation delete-stack --stack-name ${NAME}-vpc
