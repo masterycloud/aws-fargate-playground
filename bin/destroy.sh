172e50d970e2a6d5c5292fdc7687b327d2e83b9d
xmcpam <jens@masterycloud.com>
2020-03-12 18:47:43 +0100

    Add regional map for load-balancer account ids

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -7,7 +7,7 @@ echo ${BUCKET_NAME}
 aws s3 rb s3://${BUCKET_NAME} --force || true
 
 aws cloudformation delete-stack --stack-name ${SERVICE}-dashboard
-aws cloudformation delete-stack --stack-name ${SERVICE}-service
+aws cloudformation delete-stack --stack-name ${SERVICE}-fargate
 aws cloudformation delete-stack --stack-name ${SERVICE}-certificate
 aws cloudformation delete-stack --stack-name ${SERVICE}-alb
 aws cloudformation delete-stack --stack-name ${SERVICE}-vpc
